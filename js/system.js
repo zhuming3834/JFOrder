90a2e16e05d319c24bf082b033a14877071ba4a6
HGDQ <zhuming3834@sina.com>
2017-01-10 17:03:47 +0800

    2
    2

---
+++ js/system.js
@@ -0,0 +1,17 @@
+/*
+ * 网络请求相关的配置
+ */
+var BASE_URL = "http://g.justfruit.cn/webapp/api/v1/";
+//var BASE_URL = "../../json/";
+// 登录接口
+var LOGIN = "login";
+
+// 服务器返回数据类型
+var DATA_TYPE = "json";
+// 接口请求时间
+var TIME_OUT = 10000;
+// 接口请求类型
+var REQUEST_TYPE = "post";
+// 请求网络异常
+var NET_ERROR = "网络异常,请稍候再试";
+
