579f0b79ee8b554db62ee4c24a299640dc089539
HGDQ <zhuming3834@sina.com>
2017-02-06 10:16:22 +0800

    3
    3

--- js/system.js
+++ js/system.js
@@ -97,3 +97,26 @@ function goHome(){
 		}
 	});
 }
+function addDate(date, days) {
+	if (days == undefined || days == '') {
+	    days = 1;
+	}
+	var date = new Date(date);
+	date.setDate(date.getDate() + days);
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	return date.getFullYear() + '-' + getFormatDate(month) + '-' + getFormatDate(day);
+}
+// 日期月份/天的显示，如果是1位数，则在前面加上'0'
+function getFormatDate(arg) {
+    if (arg == undefined || arg == '') {
+        return '';
+    }
+
+    var re = arg + '';
+    if (re.length < 2) {
+        re = '0' + re;
+    }
+
+    return re;
+}
